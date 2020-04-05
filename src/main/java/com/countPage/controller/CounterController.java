d5186b7795b6b9e31110fe39c302e42ca2d89d7f
unknown <nhamnd.hmu@gmail.com>
2020-04-06 05:57:26 +0700

    Session & Cookie

---
+++ src/main/java/com/countPage/controller/CounterController.java
@@ -0,0 +1,23 @@
+package com.countPage.controller;
+
+import com.countPage.model.Counter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@SessionAttributes("counter")
+public class CounterController {
+    @ModelAttribute("counter")
+    public Counter counter(){
+        return new Counter();
+    }
+    @GetMapping("/home")
+    public ModelAndView getIndex(@ModelAttribute("counter") Counter counter){
+        counter.increase();
+        ModelAndView modelAndView = new ModelAndView("/index");
+        return modelAndView;
+    }
+}
